4b747ef4175cb6e55997660e83a828d09ef5adba
MUSHRIFA-C <129925382+MUSHRIFA-C@users.noreply.github.com>
2023-12-02 16:14:06 +0530

    commit

--- lib/UI/weather_bloc.dart
+++ lib/UI/weather_bloc.dart
@@ -31,7 +31,15 @@ class WeatherLoaded extends WeatherState {
   List<Object?> get props => [weather];
 }
 
-class WeatherError extends WeatherState {}
+class WeatherError extends WeatherState {
+  final String error;
+
+  WeatherError({required this.error});
+
+  @override
+  List<Object?> get props => [error];
+}
+
 
 // BLoC
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
@@ -42,9 +50,14 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   @override
   Stream<WeatherState> mapEventToState(WeatherEvent event) async* {
     if (event is FetchWeather) {
+      const double latitude = 37.7749;
+      const double longitude = -122.4194;
+      const String apiKey = '21780650cc27d641b00b7aaad548b1d6';
+
       // Perform the logic to fetch weather data here
       try {
-        final weather = await weatherUseCase.getWeather();
+        final weather = await weatherUseCase.getWeather(
+            latitude, longitude, apiKey);
         yield WeatherLoaded(weather: weather);
       } catch (e) {
         yield WeatherError(error: e.toString());
